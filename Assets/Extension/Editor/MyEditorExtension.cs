dae5df5868bc75b0b8bdc0d40179f39cc6735727
heller77 <koki001230@icluud.com>
2021-10-11 19:11:50 +0900

    Asssetsフォルダー全部コミット

---
+++ Assets/Extension/Editor/MyEditorExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Extension.Editor
+{
+    public class MyEditorExtension : EditorWindow
+    {
+        [MenuItem("tools/mywindow")]
+        static void createWindow()
+        {
+            GetWindow<MyEditorExtension>("Mywindow");
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("hello world");
+
+            GUILayout.Label("hogho");
+            if (GUILayout.Button("button"))
+            {
+                Debug.Log("button on");
+            }
+        }
+    }
+}
